c78afce0c75f135c5c91e2a36d9709b1ace14974
Natanel Guterman <natanelguterman@gmail.com>
2020-12-20 16:06:27 +0200

    Added Piece class content

--- Chess Game/Piece.h
+++ Chess Game/Piece.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <iostream>
+
+class Piece
+{
+public:
+
+	// Constructor
+	Piece();
+
+	// Destructor
+	~Piece();
+
+	// Methods
+	virtual std::string moveTo(std::string coordinates) = 0;
+
+protected:
+
+	// Fields
+	std::string _type;
+	
+	// Helper Methods
+	bool checkInvalidMove();
+	virtual bool isValidStep(int x, int y) = 0;
+};
