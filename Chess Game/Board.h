e6dd9a79d4abb133d9883bee20149728aa09387a
Natanel Guterman <natanelguterman@gmail.com>
2020-12-20 16:32:05 +0200

    Added Board class content

--- Chess Game/Board.h
+++ Chess Game/Board.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <iostream>
+#include "Piece.h"
+
+
+#define WIDTH_BOARD_BLOCKS 8
+#define HEIGHT_BOARD_BLOCKS 8
+
+class Board
+{
+public:
+
+	// Constructor
+	Board();
+
+	// Destructor
+	~Board();
+
+protected:
+
+	// Fields
+	Piece* _chessBoard[WIDTH_BOARD_BLOCKS][HEIGHT_BOARD_BLOCKS];
+};
