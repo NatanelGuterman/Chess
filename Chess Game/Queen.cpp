841b2f46cc1b8916376ad9f078821dba2deb5ee9
Karina Yagobov <karinya2604@gmail.com>
2020-12-25 20:54:17 +0200

    Wrote isValidStep func in Queen class

--- Chess Game/Queen.cpp
+++ Chess Game/Queen.cpp
@@ -9,3 +9,22 @@ Queen::Queen(char type) : Piece(type)
 Queen::~Queen()
 {
 }
+
+/*
+    Function will get coordinates and will check if this move is valid by Queen's possible moves.
+    Input:
+        int (&coordinates)[4] --> The 4 coordinates: (x, y) current, (x, y) target.
+    Output:
+        bool flag --> true if the move is valid, false if not.
+*/
+
+bool Queen::isValidStep(int(&coordinates)[4])
+{
+    bool flag = false;
+    if (!(coordinates[X_CURRENT] != coordinates[X_TARGET] && coordinates[Y_CURRENT] != coordinates[Y_TARGET]) ||
+        coordinates[X_CURRENT] - coordinates[X_TARGET] == coordinates[Y_CURRENT] - coordinates[Y_TARGET])
+    {
+        flag = true;
+    }
+	return flag;
+}
