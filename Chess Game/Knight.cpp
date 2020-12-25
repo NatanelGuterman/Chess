867c6ede59466d43cb2b15b2f254542ef6118a62
Natanel Guterman <natanelguterman@gmail.com>
2020-12-25 23:39:47 +0200

    Added Knight isValidStep func

--- Chess Game/Knight.cpp
+++ Chess Game/Knight.cpp
@@ -1,5 +1,7 @@
 #include "Knight.h"
 
+
+
 // Constructor
 Knight::Knight(char type) : Piece(type)
 {
@@ -8,4 +10,25 @@ Knight::Knight(char type) : Piece(type)
 // Destructor
 Knight::~Knight()
 {
+}
+
+
+/*
+	Function will check if the knight instance can move to the given target coordinates.
+	Input:
+		int(&coordinates)[AMOUNT_OF_COORD] - the array coords, [x1, y1, x2 (t) , y2 (t)]
+	Output:
+		bool - true if the knight instance van move to the target coords, else false.
+*/
+
+bool Knight::isValidStep(int(&coordinates)[AMOUNT_OF_COORD]) const
+{
+	bool flag = false;
+	if (abs(coordinates[X_CURRENT] - coordinates[X_TARGET]) != abs(coordinates[Y_CURRENT] - coordinates[Y_TARGET]) && 
+		(abs(coordinates[Y_CURRENT] - coordinates[Y_TARGET]) == DIFFRENCES_MIN_ABS || abs(coordinates[Y_CURRENT] - coordinates[Y_TARGET]) == DIFFRENCES_MAX_ABS) && 
+		(abs(coordinates[X_CURRENT] - coordinates[X_TARGET]) == DIFFRENCES_MIN_ABS || abs(coordinates[X_CURRENT] - coordinates[X_TARGET]) == DIFFRENCES_MAX_ABS))
+	{
+		flag = true;
+	}
+	return flag;
 }
--- Chess Game/Knight.h
+++ Chess Game/Knight.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "Piece.h"
 
+#define DIFFRENCES_MAX_ABS 2
+#define DIFFRENCES_MIN_ABS 1
+
 class Knight : public Piece
 {
 private:
 	// Methods
-	virtual bool isValidStep(int(&coordinates)[4]);
+	virtual bool isValidStep(int(&coordinates)[AMOUNT_OF_COORD]) const;
 
 public:
 	// Constructor
