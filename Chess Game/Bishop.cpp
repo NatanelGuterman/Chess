f55b55a4ef89f9802574b4b664eb52c608c5592f
Karina Yagobov <karinya2604@gmail.com>
2020-12-25 20:53:57 +0200

    Wrote isValidStep func in Bishop class

--- Chess Game/Bishop.cpp
+++ Chess Game/Bishop.cpp
@@ -9,3 +9,21 @@ Bishop::Bishop(char type) : Piece(type)
 Bishop::~Bishop()
 {
 }
+
+/*
+    Function will get coordinates and will check if this move is valid by Bishop's possible moves.
+    Input:
+        int (&coordinates)[4] --> The 4 coordinates: (x, y) current, (x, y) target.
+    Output:
+        bool flag --> true if the move is valid, false if not.
+*/
+
+bool Bishop::isValidStep(int(&coordinates)[4])
+{
+	bool flag = false;
+    if (coordinates[X_CURRENT] - coordinates[X_TARGET] == coordinates[Y_CURRENT] - coordinates[Y_TARGET])
+    {
+        flag = true;
+    }
+    return flag;
+}
