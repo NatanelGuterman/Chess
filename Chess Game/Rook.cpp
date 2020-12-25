003c1afb51cc64279aac6a7acd39bf4648b2dc43
Karina Yagobov <karinya2604@gmail.com>
2020-12-25 20:53:31 +0200

    Wrote isValidStep func in Rook class

--- Chess Game/Rook.cpp
+++ Chess Game/Rook.cpp
@@ -9,3 +9,21 @@ Rook::Rook(char type) : Piece(type)
 Rook::~Rook()
 {
 }
+
+/*
+    Function will get coordinates and will check if this move is valid by Rook's possible moves.
+    Input:
+        int (&coordinates)[4] --> The 4 coordinates: (x, y) current, (x, y) target.
+    Output:
+        bool flag --> true if the move is valid, false if not.
+*/
+
+bool Rook::isValidStep(int(&coordinates)[4])
+{
+    bool flag = true;
+    if (coordinates[X_CURRENT] != coordinates[X_TARGET] && coordinates[Y_CURRENT] != coordinates[Y_TARGET])
+    {
+        flag = false;
+    }
+    return flag;
+}
