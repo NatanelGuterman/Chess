d732402315695c5bc17f46048ad3f98c4dc54467
Karina Yagobov <karinya2604@gmail.com>
2020-12-27 22:57:55 +0200

    Fixed isValidStep in Bishop class

--- Chess Game/Bishop.cpp
+++ Chess Game/Bishop.cpp
@@ -20,9 +20,49 @@ Bishop::~Bishop()
 
 bool Bishop::isValidStep(int(&coordinates)[AMOUNT_OF_COORD]) const
 {
-    if (coordinates[X_CURRENT] - coordinates[X_TARGET] == coordinates[Y_CURRENT] - coordinates[Y_TARGET])
+    int x = 0, y = 0, lastCoordToCheck = coordinates[X_TARGET], xFactor = 0, yFactor = 0;
+
+    if (abs(coordinates[X_CURRENT] - coordinates[X_TARGET]) != abs(coordinates[Y_CURRENT] - coordinates[Y_TARGET]))
     {
-        return true;
+        return false;
     }
-    return false;
+
+    if (coordinates[Y_CURRENT] < coordinates[Y_TARGET]) // move right
+    {
+        if (coordinates[X_CURRENT] < coordinates[X_TARGET]) // up right direction
+        {
+            xFactor = yFactor = 1;
+        }
+        else // up left direction
+        {
+
+            xFactor = -1;
+            yFactor = 1;
+        }
+    }
+    else // move left
+    {
+        if (coordinates[X_CURRENT] < coordinates[X_TARGET]) // down right direction
+        {
+            xFactor = 1;
+            yFactor = -1;
+        }
+        else // down left direction
+        {
+            xFactor = yFactor = -1;
+        }
+    }
+
+    x = coordinates[X_CURRENT] + xFactor;
+    y = coordinates[Y_CURRENT] + yFactor;
+    while (x != lastCoordToCheck)
+    {
+        if (Board::_chessBoard[x][y] != NULL)
+        {
+            return false;
+        }
+        x += xFactor;
+        y += yFactor;
+    }
+    return true;
 }
