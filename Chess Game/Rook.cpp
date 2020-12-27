bf1d265c79675171597cf7d05bab8d23eb3e7efe
Karina Yagobov <karinya2604@gmail.com>
2020-12-27 21:41:24 +0200

    Fixed isValidStep in Rook class

--- Chess Game/Rook.cpp
+++ Chess Game/Rook.cpp
@@ -20,9 +20,52 @@ Rook::~Rook()
 
 bool Rook::isValidStep(int(&coordinates)[AMOUNT_OF_COORD]) const
 {
+    int changedCoord = 0, lastCoordToCheck = 0;
     if (coordinates[X_CURRENT] != coordinates[X_TARGET] && coordinates[Y_CURRENT] != coordinates[Y_TARGET])
     {
         return false;
+    }
+    if (coordinates[X_CURRENT] != coordinates[X_TARGET])
+    {
+        if (coordinates[X_CURRENT] < coordinates[X_TARGET])
+        {
+            changedCoord = coordinates[X_CURRENT] + 1;
+            lastCoordToCheck = coordinates[X_TARGET];
+        }
+        else
+        {
+            changedCoord = coordinates[X_TARGET] + 1;
+            lastCoordToCheck = coordinates[X_CURRENT];
+        }
+        while (changedCoord != lastCoordToCheck)
+        {
+            if (Board::_chessBoard[changedCoord][coordinates[Y_CURRENT]] != NULL)
+            {
+                return false;
+            }
+            changedCoord++;
+        }
+    }
+    else
+    {
+        if (coordinates[Y_CURRENT] < coordinates[Y_TARGET])
+        {
+            changedCoord = coordinates[Y_CURRENT] + 1;
+            lastCoordToCheck = coordinates[Y_TARGET];
+        }
+        else
+        {
+            changedCoord = coordinates[Y_TARGET] + 1;
+            lastCoordToCheck = coordinates[Y_CURRENT];
+        }
+        while (changedCoord != lastCoordToCheck)
+        {
+            if (Board::_chessBoard[coordinates[X_CURRENT]][changedCoord] != NULL)
+            {
+                return false;
+            }
+            changedCoord++;
+        }
     }
     return true;
 }
