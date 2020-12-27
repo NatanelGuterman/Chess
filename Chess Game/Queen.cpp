a7aa0d5c1aef7593ae0079579b6cd6bf4ce29946
Karina Yagobov <karinya2604@gmail.com>
2020-12-28 00:32:31 +0200

    Tried to fix Queen isValidStep

--- Chess Game/Queen.cpp
+++ Chess Game/Queen.cpp
@@ -20,10 +20,113 @@ Queen::~Queen()
 
 bool Queen::isValidStep(int(&coordinates)[AMOUNT_OF_COORD]) const
 {
-    if (!(coordinates[X_CURRENT] != coordinates[X_TARGET] && coordinates[Y_CURRENT] != coordinates[Y_TARGET]) ||
-        coordinates[X_CURRENT] - coordinates[X_TARGET] == coordinates[Y_CURRENT] - coordinates[Y_TARGET])
+    if (validRook(coordinates) || validBishop(coordinates))
     {
         return true;
     }
 	return false;
+}
+
+// Using the Rook && Bishop to check if the Queen moves right (Queen's move is Rook's and Bishop's move together).
+
+bool Queen::validRook(int(&coordinates)[AMOUNT_OF_COORD]) const
+{
+    int changedCoord = 0, lastCoordToCheck = 0;
+
+    if (coordinates[X_CURRENT] != coordinates[X_TARGET] && coordinates[Y_CURRENT] != coordinates[Y_TARGET])
+    {
+        return false;
+    }
+    if (coordinates[X_CURRENT] != coordinates[X_TARGET])
+    {
+        if (coordinates[X_CURRENT] < coordinates[X_TARGET])
+        {
+            changedCoord = coordinates[X_CURRENT] + 1;
+            lastCoordToCheck = coordinates[X_TARGET];
+        }
+        else
+        {
+            changedCoord = coordinates[X_TARGET] + 1;
+            lastCoordToCheck = coordinates[X_CURRENT];
+        }
+        while (changedCoord != lastCoordToCheck)
+        {
+            if (Board::_chessBoard[changedCoord][coordinates[Y_CURRENT]] != NULL)
+            {
+                return false;
+            }
+            changedCoord++;
+        }
+    }
+    else
+    {
+        if (coordinates[Y_CURRENT] < coordinates[Y_TARGET])
+        {
+            changedCoord = coordinates[Y_CURRENT] + 1;
+            lastCoordToCheck = coordinates[Y_TARGET];
+        }
+        else
+        {
+            changedCoord = coordinates[Y_TARGET] + 1;
+            lastCoordToCheck = coordinates[Y_CURRENT];
+        }
+        while (changedCoord != lastCoordToCheck)
+        {
+            if (Board::_chessBoard[coordinates[X_CURRENT]][changedCoord] != NULL)
+            {
+                return false;
+            }
+            changedCoord++;
+        }
+    }
+    return true;
+}
+
+bool Queen::validBishop(int(&coordinates)[AMOUNT_OF_COORD]) const
+{
+    int x = 0, y = 0, lastCoordToCheck = coordinates[X_TARGET], xFactor = 0, yFactor = 0;
+
+    if (abs(coordinates[X_CURRENT] - coordinates[X_TARGET]) != abs(coordinates[Y_CURRENT] - coordinates[Y_TARGET]))
+    {
+        return false;
+    }
+
+    if (coordinates[Y_CURRENT] < coordinates[Y_TARGET]) // move right
+    {
+        if (coordinates[X_CURRENT] < coordinates[X_TARGET]) // up right direction
+        {
+            xFactor = yFactor = 1;
+        }
+        else // up left direction
+        {
+
+            xFactor = -1;
+            yFactor = 1;
+        }
+    }
+    else // move left
+    {
+        if (coordinates[X_CURRENT] < coordinates[X_TARGET]) // down right direction
+        {
+            xFactor = 1;
+            yFactor = -1;
+        }
+        else // down left direction
+        {
+            xFactor = yFactor = -1;
+        }
+    }
+
+    x = coordinates[X_CURRENT] + xFactor;
+    y = coordinates[Y_CURRENT] + yFactor;
+    while (x != lastCoordToCheck)
+    {
+        if (Board::_chessBoard[x][y] != NULL)
+        {
+            return false;
+        }
+        x += xFactor;
+        y += yFactor;
+    }
+    return true;
 }
--- Chess Game/Queen.h
+++ Chess Game/Queen.h
@@ -7,6 +7,10 @@ private:
 	// Methods
 	virtual bool isValidStep(int(&coordinates)[AMOUNT_OF_COORD]) const;
 
+	// Using the Rook && Bishop to check if the Queen moves right (Queen's move is Rook's and Bishop's move together).
+	bool validRook(int(&coordinates)[AMOUNT_OF_COORD]) const;
+	bool validBishop(int(&coordinates)[AMOUNT_OF_COORD]) const;
+
 public:
 	// Constructor
 	Queen(char type);
